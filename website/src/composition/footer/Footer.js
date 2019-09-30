4c3fde3fab0039aa76efe1221eb7050599b800d0
eskilsen <eskil_hognestad@hotmail.com>
2019-09-30 12:12:14 +0200

    init commit

---
+++ website/src/composition/footer/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+
+    render() {
+        return (
+            <div className="footer">
+                <div className="footerElements">
+                    <p>This is a footer</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ website/src/pages/home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './Home.css';
+
+class Home extends Component {
+    
+    render() {
+
+        return (
+            <div className="wholeHome">
+                <div className="body">
+                    <p>Her kommer en haug av informasjon as fyfaen <br/>få det på da</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
