f715e5c0ced7e8046d0f4d96b3c1a1df8a9798cf
eskilsen <eskil_hognestad@hotmail.com>
2019-09-30 16:28:00 +0200

    added footer

--- website/src/composition/footer/Footer.js
+++ website/src/composition/footer/Footer.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import './Footer.css';
 
+import igLogo from '../../assets/footer/ig.png';
+import faceLogo from '../../assets/footer/faceLogo.png';
+
 class Footer extends React.Component {
 
     render() {
         return (
+
             <div className="footer">
                 <div className="footerElements">
-                    <p>This is a footer</p>
+                    
+                    <div className="SoMe">
+                        <a href = "https://www.instagram.com/eidsvoldturn/">
+                        <img className="instagram" src={igLogo} />
+                        </a>
+                        <a href="https://www.facebook.com/eidsvoldturnfotball/">
+                            <img className="facebook" src = {faceLogo} />
+
+                        </a>
+                    </div>
+
+                    <div className="madeBy">
+                        <p>
+                        Made by:<br/>
+                        Nicolai C. Michelet<br/>
+                        Eskil Hognestad
+                        </p>
+                    </div>
                 </div>
+
             </div>
         );
     }
