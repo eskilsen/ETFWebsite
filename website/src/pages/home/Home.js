61736f0b83079954c6bd167b7fc637196131d5e4
eskilsen <eskil_hognestad@hotmail.com>
2019-10-03 17:55:36 +0200

    started styling on homepage with highlighted news and the newslist

--- website/src/pages/home/Home.js
+++ website/src/pages/home/Home.js
@@ -12,47 +12,80 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            firstArticle: this.props.articles[0],
         }
-      }
+    }
+
 
     newsDisplayer = () =>(
-        this.props.articles.map((article,id) => 
+        this.props.articles.filter((article,id) => {
+            return id !== 0
+        }).map((article,id) => {
 
-        <tr> 
+            return(
+                <tr> 
+                    <td key={id}>
+                        
+                    <Link to = {{
+                        pathname:`/news`,
+                        state: {
+                            article: article
+                        }
+                    }}
+                    >
+                    {article.title != "" ? article.title: "ingen nyhet"}</Link>
+                    </td>
+                </tr>
             
-            <td key={id}>
-                
-            <Link to = {{
-                pathname:`/news`,
-                state: {
-                    article: article
-                }
-            }}
-            >
-            {article.title != "" ? article.title: "ingen nyhet"}</Link>
-            </td>
-        </tr>
-        )
+            )
+        })
     );
 
     render() {
+        let imageURL;
+        let title;
+        let firstArticle;
+        if(this.state.firstArticle){
+            imageURL = this.state.firstArticle.urlToImage;
+            title = this.state.firstArticle.title;
+            firstArticle = this.state.firstArticle;
+        }
+        
+
 
 <li><Link className="link" to="/">Home</Link></li>
         console.log(this.props.articles)
         return (
             <div className="home">
                 <div className="highlighted">
-                    <p>hei </p>
+                <Link to = {{
+                        pathname:`/news`,
+                        state: {
+                            article: firstArticle,
+                        }
+                    }}
+                    >
+                    
+                    <img src={imageURL}/>
+                    <div className="highlightedTitle">{title}</div>
+                </Link>
                 </div>
 
                 <div className="news">
                     <div className="newsList">
                         <table>
-                            <tr>
-                                <th>Nyheter</th>
-                            </tr>
+
+                            
                             {this.newsDisplayer()}
                         </table>
+                    </div>
+                </div>
+                <div className="sponsors">
+                    <div className="mainSponsors">
+                        <p>HOVED</p>
+                    </div>
+                    <div className="otherSponsors">
+                        <p>Andre</p>
                     </div>
                 </div>
             </div>
