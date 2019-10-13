0077992b2936d7745eb2df367ab70162acbd43c1
eskilsen <eskil_hognestad@hotmail.com>
2019-10-13 19:17:53 +0200

    added sidebar if we need

--- website/src/pages/news/News.js
+++ website/src/pages/news/News.js
@@ -9,23 +9,32 @@ class News extends Component {
     render() {
         let article = this.props.location.state.article;
         let title = article.title;
-        let source = article.source;
+        let source = article.source.name;
         let content = article.content;
         let published = article.publishedAt;
         let sourceURL = article.url;
         let imageURL = article.urlToImage;
+        let author = article.author;
         return (
             <div className="gridContainer">
                 <div className="newsHeader">
                     <h3>{title}</h3>
                 </div>
 
+                <div className="sidebar">
+                    <a>Hjem</a>
+                    <a>Bort</a>
+                    <a>Uavgjort</a>
+                    <a>Hehe</a>
+                </div>
+
                 <div className="picture">
-                    <img src={imageURL} alt="image"/>
+                    <img id="newsPicture" src={imageURL} alt="image"/>
                 </div>
 
                 <div className="information">
-                  {content}
+                  <p>{content}</p> <br/>
+                  hentet fra: {sourceURL}
                 </div>
                 
 
